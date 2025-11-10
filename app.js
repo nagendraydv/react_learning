e2c97c817fe03ff0316bfe9a63e4c4ebcd9e65ae
nagendra <98nagendra@gmail.com>
2025-11-10 23:50:01 +0530

    react day 1

---
+++ app.js
@@ -0,0 +1,3 @@
+const element = React.createElement('h1', {}, "Hello Indian Army");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
--- first.js
+++ first.js
@@ -3,7 +3,16 @@ const React = {
     createElement: function(tag, styles, children){
 
         const element = document.createElement(tag)
-        element.innerText = children
+        if (typeof children === 'object'){
+            for(let value of children){
+                element.append(value)
+            }
+
+        }
+        else{
+            element.innerText = children
+        }
+        
 
         for(let key in styles){
             element.style[key] = styles[key]
@@ -12,6 +21,13 @@ const React = {
 
     }
 
+}
+
+
+const ReactDom = {
+    render : function(element, root){
+        root.append(element)
+    }
 }
 
 
@@ -28,9 +44,28 @@ const React = {
 // header2.style.backgroundColor = 'black'
 // header2.style.fontSize = '10px'
 
-const header1 = React.createElement('h1', {fontSize:'20px', backgroundColor: 'blue', color: "white"}, "Hello Nagendra ji")
+
+// React : Object
+// ReactDom : Object
+
+const header1 = React.createElement('h1', {fontSize:'20px', backgroundColor: 'blue', color: "white"}, "Hello Nagendra jiiiii")
 const header2 = React.createElement('h1', {fontSize:'30px', backgroundColor: 'black', color: "white"}, "Aap bahut bade aadmi ho..")
 
-const root = document.getElementById('root')
-root.append(header1)
-root.append(header2)
+// const root = document.getElementById('root')
+// root.append(header1)
+// root.append(header2)
+
+// unordered list
+// html
+// css
+// js
+
+const li1 = React.createElement('li', {}, 'HTML')
+const li2 = React.createElement('li', {}, 'CSS')
+const li3 = React.createElement('li', {}, 'JS')
+const UL = React.createElement('ul', {fontSize:'20px', backgroundColor: 'blue', color: "white"}, [li1, li2, li3])
+
+
+ReactDom.render(header1, document.getElementById('root'))
+ReactDom.render(header2, document.getElementById('root'))
+ReactDom.render(UL, document.getElementById('root'))
